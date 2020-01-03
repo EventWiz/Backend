cff7fe17bd3a5d4f9684491d15da8f9e4dc97ee0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-03 12:05:45 +0100

    Removed old validations

--- validators/rules/session.js
+++ validators/rules/session.js
@@ -1,36 +1,5 @@
 import { body } from 'express-validator';
 
-// export const createSessionValidationRules = () => [
-//   body('event_id')
-//     .not()
-//     .isEmpty()
-//     .withMessage('event_id is required'),
-//   body('start_time')
-//     .not()
-//     .isEmpty()
-//     .withMessage('start_time is required'),
-//   body('end_time')
-//     .not()
-//     .isEmpty()
-//     .withMessage('end_time is required'),
-//   body('speaker')
-//     .not()
-//     .isEmpty()
-//     .withMessage('speaker is required'),
-//   body('topic')
-//     .not()
-//     .isEmpty()
-//     .withMessage('topic is required'),
-//   body('venue')
-//     .not()
-//     .isEmpty()
-//     .withMessage('venue is required'),
-//   body('date')
-//     .not()
-//     .isEmpty()
-//     .withMessage('date is required'),
-// ];
-
 export const createSessionValidationRules = () => [
   body().isArray(),
   body('*.event_id', 'event_id is required')
