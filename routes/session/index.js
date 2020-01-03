664f909524161d05a7a0d4d102069d332eab1187
Rexben001 <rexben.rb@gmail.com>
2020-01-03 13:09:59 +0100

    edit and delete session endpoints

--- routes/session/index.js
+++ routes/session/index.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import createSessionValidationRules from '../../validators/rules/session';
 import validate from '../../validators';
+import {
+  createSession,
   getSessionById,
+  deleteSession,
+  editSession,
+} from '../../controllers/sessions';
 import isAuthenticated from '../../middleware/auth';
 
 const router = Router();
@@ -14,5 +19,10 @@ router.post(
   createSession,
 );
 
+router.delete('/:sessionId', isAuthenticated, deleteSession);
+
+router.put('/:sessionId', isAuthenticated, editSession);
+
 router.get('/:sessionId', getSessionById);
+
 export default router;
