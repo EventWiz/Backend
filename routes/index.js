8c4077ac37ec8a64bf77f92da4db3dec981987bf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-03 10:35:29 +0100

    connected the session route to the main route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import authRoutes from './auth';
 import eventRoutes from './event';
+import sessionRoutes from './session';
 
 const router = Router();
 
 router.get('/', (req, res) => res.status(200).json('Welcome'));
 router.use('/auth', authRoutes);
 router.use('/events', eventRoutes);
+router.use('/sessions/', sessionRoutes);
 
 export default router;
