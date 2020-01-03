b230785d1711f4645516aa9ceaf02a13f5fb10a6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-03 10:21:01 +0100

    Creates a session

---
+++ controllers/sessions.js
@@ -0,0 +1,31 @@
+import formatResponse from '../helpers';
+
+import models from '../database/models';
+
+export default async function createEvent(req, res, next) {
+  try {
+    const {
+      event_id,
+      start_date,
+      end_date,
+      speaker,
+      topic,
+      venue,
+      date,
+    } = req.body;
+
+    const session = await models.Session.create({
+      event_id,
+      start_date,
+      end_date,
+      speaker,
+      topic,
+      venue,
+      date,
+    });
+
+    return formatResponse(res, { message: 'success', session }, 201);
+  } catch (error) {
+    next(error);
+  }
+}
