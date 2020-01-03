e1585c731e34cb79198068ac137ba4368a146695
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-01-03 10:07:07 +0100

    Fix: handle error

--- controllers/user.js
+++ controllers/user.js
@@ -5,22 +5,26 @@ import formatResponse from '../helpers';
 import models from '../database/models';
 
 
-async function getUserProfile(req, res) {
-  const user = await models.User.findOne({
-    where: { id: req.user.id },
-    include: [
-      {
-        model: models.Event,
-        as: 'events',
-      },
-      {
-        model: models.Event,
-        as: 'rsvps',
-        through: { attributes: ['ticket_id'] },
-      },
-    ],
-  });
-  return formatResponse(res, { user });
+async function getUserProfile(req, res, next) {
+  try {
+    const user = await models.User.findOne({
+      where: { id: req.user.id },
+      include: [
+        {
+          model: models.Event,
+          as: 'events',
+        },
+        {
+          model: models.Event,
+          as: 'rsvps',
+          through: { attributes: ['ticket_id'] },
+        },
+      ],
+    });
+    return formatResponse(res, { user });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export default getUserProfile;
