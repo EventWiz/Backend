dc56af38d2e52f80c6271d262dacbf2505e1c67f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-03 11:33:21 +0100

    New rules to validate array of objects

--- validators/rules/session.js
+++ validators/rules/session.js
@@ -1,32 +1,51 @@
 import { body } from 'express-validator';
 
+// export const createSessionValidationRules = () => [
+//   body('event_id')
+//     .not()
+//     .isEmpty()
+//     .withMessage('event_id is required'),
+//   body('start_time')
+//     .not()
+//     .isEmpty()
+//     .withMessage('start_time is required'),
+//   body('end_time')
+//     .not()
+//     .isEmpty()
+//     .withMessage('end_time is required'),
+//   body('speaker')
+//     .not()
+//     .isEmpty()
+//     .withMessage('speaker is required'),
+//   body('topic')
+//     .not()
+//     .isEmpty()
+//     .withMessage('topic is required'),
+//   body('venue')
+//     .not()
+//     .isEmpty()
+//     .withMessage('venue is required'),
+//   body('date')
+//     .not()
+//     .isEmpty()
+//     .withMessage('date is required'),
+// ];
+
 export const createSessionValidationRules = () => [
-  body('event_id')
-    .not()
-    .isEmpty()
-    .withMessage('event_id is required'),
-  body('start_time')
-    .not()
-    .isEmpty()
-    .withMessage('start_time is required'),
-  body('end_time')
-    .not()
-    .isEmpty()
-    .withMessage('end_time is required'),
-  body('speaker')
-    .not()
-    .isEmpty()
-    .withMessage('speaker is required'),
-  body('topic')
-    .not()
-    .isEmpty()
-    .withMessage('topic is required'),
-  body('venue')
-    .not()
-    .isEmpty()
-    .withMessage('venue is required'),
-  body('date')
-    .not()
-    .isEmpty()
-    .withMessage('date is required'),
+  body().isArray(),
+  body('*.event_id', 'event_id is required')
+    .exists()
+    .isString(),
+  body('*.start_time', 'start_time is required').exists(),
+  body('*.end_time', 'end_time is required').exists(),
+  body('*.speaker', 'speaker is required')
+    .exists()
+    .isString(),
+  body('*.topic', 'topic is required')
+    .exists()
+    .isString(),
+  body('*.venue', 'venue is required')
+    .exists()
+    .isString(),
+  body('*.date', 'date is required').exists(),
 ];
