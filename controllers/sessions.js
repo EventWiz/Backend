ac8b3107d8d4d21f4042e8a76ababe121acbf4c1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-03 12:03:55 +0100

    creates the sessions from an array of sessions

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,27 +4,36 @@ import models from '../database/models';
 
 export async function createSession(req, res, next) {
   try {
-    const {
-      event_id,
-      start_time,
-      end_time,
-      speaker,
-      topic,
-      venue,
-      date,
-    } = req.body;
+    const sessions = req.body.map(async body => {
+      const {
+        event_id,
+        start_time,
+        end_time,
+        speaker,
+        topic,
+        venue,
+        date,
+      } = body;
 
-    const session = await models.Session.create({
-      event_id,
-      start_time,
-      end_time,
-      speaker,
-      topic,
-      venue,
-      date,
+      const session = await models.Session.create({
+        event_id,
+        start_time,
+        end_time,
+        speaker,
+        topic,
+        venue,
+        date,
+      });
+
+      return session.dataValues;
     });
+    const createdSessions =  await Promise.all(sessions);
 
-    return formatResponse(res, { message: 'success', session }, 201);
+    return formatResponse(
+      res,
+      { message: `${sessions.length} session[s] created successfully.`, data: createdSessions },
+      201,
+    );
   } catch (error) {
     next(error);
   }
